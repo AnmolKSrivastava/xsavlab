dd44f2332bc9a325ae34d4e0e1e9b0f5b546bc83
Anmol Kumar Srivastava <anmolsrivastava25@gmail.com>
2026-04-17 15:35:39 +0530

    security: move Firebase config to environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// TODO: Replace with your Firebase project configuration
-// You can find these values in Firebase Console > Project Settings > General > Your apps
 const firebaseConfig = {
-  apiKey: "AIzaSyDbIuV7Lbc7Sth14bqhgJRjlFY6sTZh_8c",
-  authDomain: "xsavlab.firebaseapp.com",
-  projectId: "xsavlab",
-  storageBucket: "xsavlab.firebasestorage.app",
-  messagingSenderId: "400044598541",
-  appId: "1:400044598541:web:6bbc431cf641c8edeabc44"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
